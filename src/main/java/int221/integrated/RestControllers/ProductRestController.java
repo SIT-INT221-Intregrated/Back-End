badc3f5d2552ed742e2ac840ad73a6ecc75774cd
Possawat   Tanam <possawat.tanam@mail.kmutt.ac.th>
2021-05-03 14:49:17 +0700

    064 [Edit] Add Delete API

--- src/main/java/int221/integrated/RestControllers/ProductRestController.java
+++ src/main/java/int221/integrated/RestControllers/ProductRestController.java
@@ -17,33 +17,20 @@ public class ProductRestController {
 		return productsJpaRepository.findAll();
 	}
 
-	@GetMapping("/products/{productCode}")
-	public Products showProduct(@PathVariable String productCode) {
-		Products product = this.productsJpaRepository.findById(productCode).orElse(null);
-		return product;
-	}
-
-	@PostMapping("/products")
-	public Products createProduct(@RequestBody Products newProduct) {
-		return productsJpaRepository.save(newProduct);
-	}
-
-	@DeleteMapping("/product/{productCode}")
-	public String deleteProduct(@PathVariable String productCode) {
-		productsJpaRepository.deleteById(productCode);
-		return "delete product success";
+	@GetMapping("/products/{productcode}")
+	public Products showProduct(@PathVariable String productcode) {
+		return this.productsJpaRepository.findById(productcode).orElse(null);
 	}
 
 	/*
-	 * @PostMapping("/create") public Products createProduct(@RequestBody Products
-	 * newProduct) { return productsJpaRepository.save(newProduct); }
-	 * 
-	 * @RequestMapping("/create") public String create(Model model) { return
-	 * "create"; }
-	 * 
-	 * @GetMapping("/save") public String save(@RequestBody Products newProduct) {
-	 * productsJpaRepository.save(newProduct); // newProduct.("product",
-	 * newProduct); return "redirect:/show/" + newProduct.getProductCode(); }
+	 * @PostMapping("/products/{productcode}") public Products
+	 * createProduct(@RequestBody Products newProduct) { return
+	 * productsJpaRepository.save(newProduct); }
 	 */
 
+	@DeleteMapping("/products/{productcode}")
+	public String deleteProduct(@PathVariable String productcode) {
+		productsJpaRepository.deleteById(productcode);
+		return "Delete Product" + " " + productcode + " " + "Success";
+	}
 }
--- src/main/java/int221/integrated/ShoesWebsiteApplication.java
+++ src/main/java/int221/integrated/ShoesWebsiteApplication.java
@@ -9,6 +9,4 @@ public class ShoesWebsiteApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ShoesWebsiteApplication.class, args);
 	}
-
-	
 }
